2f57c84597fe976703fe3a89e7a22f551393d422
Reinis RR <reinis.ramis@gmail.com>
2023-05-28 15:04:35 +0300

    Task1 done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,39 +23,75 @@ class DatabaseSeeder extends Seeder
         $france = Country::where('name', 'France')->first();
         $renault = new Manufacturer();
         $renault->name = 'Renault';
+        $renault->founded = 1898;
+        $renault->website = 'https://www.renault.lv';
         $france->manufacturers()->save($renault);
 
         #approach #2 - use "create" shortcut of collection
-        $france->manufacturers()->create(['name' => 'Peugeot']);
+        $france->manufacturers()->create([
+            'name' => 'Peugeot',
+            'founded' => 1896,
+            'website' => 'https://www.peugeot.lv/lv/'
+        ]);
 
         $spain = Country::where('name', 'Spain')->first();
         $japan = Country::where('name', 'Japan')->first();
-        $spain->manufacturers()->create(['name' => 'Seat']);
-        $japan->manufacturers()->create(['name' => 'Toyota']);
-        $france->manufacturers()->create(['name' => 'Peugeot']);
+        $spain->manufacturers()->create([
+            'name' => 'Seat',
+            'founded' => 1950,
+            'website' => 'https://www.seat.lv/home'
+        ]);
+        $japan->manufacturers()->create([
+            'name' => 'Toyota',
+            'founded' => 1937,
+            'website' => 'https://www.toyota.lv/'
+        ]);
+        // $france->manufacturers()->create([
+        //     'name' => 'Peugeot',
+        //     'founded' => 1896,
+        //     'website' => 'https://www.peugeot.lv/lv/'
+        // ]);
 
         #approach #3 - calling "associate" on sub-object
         $germany = Country::where('name', 'Germany')->first();
-        $germany->manufacturers()->create(['name' => 'Volkswagen']);
+        $germany->manufacturers()->create([
+            'name' => 'Volkswagen',
+            'founded' => 1937,
+            'website' => 'https://www.volkswagen.lv/'
+        ]);
 
         $volkswagen = Manufacturer::where('name', 'Volkswagen')->first();
-        $volkswagen->carmodels()->create(['name' => 'Passat']);
-        $volkswagen->carmodels()->create(['name' => 'Golf']);
-        $volkswagen->carmodels()->create(['name' => 'Multivan']);
+        $volkswagen->carmodels()->create([
+            'name' => 'Passat',
+            'production_started' => 1973,
+            'min_price' => 28.570
+        ]);
+        $volkswagen->carmodels()->create([
+            'name' => 'Golf',
+            'production_started' => 1974,
+            'min_price' => 23.195
+        ]);
+        $volkswagen->carmodels()->create([
+            'name' => 'Multivan',
+            'production_started' => 2009,
+            'min_price' => 66.490
+        ]);
 
         $audi = new Manufacturer();
         $audi->name = 'Audi';
+        $audi->founded = 1909;
+        $audi->website = 'https://www.audi.lv';
         $audi->country()->associate($germany);
         $audi->save();
 
-        $audi->carmodels()->create(['name' => 'A4']);
-        $audi->carmodels()->create(['name' => 'A6']);
-        $audi->carmodels()->create(['name' => 'Q3']);
-        $audi->carmodels()->create(['name' => 'Q4']);
-        $audi->carmodels()->create(['name' => 'Q5']);
+        $audi->carmodels()->create(['name' => 'A4', 'production_started' => 2009, 'min_price' => 66.490]);
+        $audi->carmodels()->create(['name' => 'A6', 'production_started' => 2009, 'min_price' => 66.490]);
+        $audi->carmodels()->create(['name' => 'Q3', 'production_started' => 2009, 'min_price' => 66.490]);
+        $audi->carmodels()->create(['name' => 'Q4', 'production_started' => 2009, 'min_price' => 66.490]);
+        $audi->carmodels()->create(['name' => 'Q5', 'production_started' => 2009, 'min_price' => 66.490]);
 
         $seat = Manufacturer::where('name', 'Seat')->first();
-        $seat->carmodels()->create(['name' => 'Toledo']);
-        $seat->carmodels()->create(['name' => 'Ibiza']);        
+        $seat->carmodels()->create(['name' => 'Toledo', 'production_started' => 2009, 'min_price' => 66.490]);
+        $seat->carmodels()->create(['name' => 'Ibiza', 'production_started' => 2009, 'min_price' => 66.490]);
     }
 }
